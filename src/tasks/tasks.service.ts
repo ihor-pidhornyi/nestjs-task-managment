b81b2d3eba77802d217d18584175ee5993e48082
ihorpidhornyi <ihorp@rabota.ua>
2021-09-19 20:23:41 +0300

    create task and get task by id templates

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-  private readonly tasks: Task[] = [];
+  private readonly tasks: Task[] = [
+    {
+      id: '2146c9b9-9f05-4846-9d9e-2f1e7edbd7e3',
+      title: 'Clean my room',
+      description: 'Lots of cleaning has to be done',
+      status: TaskStatus.OPEN,
+    },
+  ];
 
   public getAllTasks(): Task[] {
     return this.tasks;
   }
+
+  public getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
 }
