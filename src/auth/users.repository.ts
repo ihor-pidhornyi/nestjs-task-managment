581977c3e935eb5ff0871da3678aaf7dfe9e9b39
ihorpidhornyi <ihorp@rabota.ua>
2021-10-30 22:30:08 +0300

    feat(auth): added hashing passwords including salt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,11 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { User } from './user.entity';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
@@ -13,7 +14,10 @@ export class UsersRepository extends Repository<User> {
   ): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
