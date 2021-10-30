63ab0b580905d2cc1e8e923d2a2dbd33b57621bb
ihorpidhornyi <ihorp@rabota.ua>
2021-10-30 22:59:40 +0300

    feat(auth): added sign in feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
-  ) {}
+  ) {
+  }
 
   public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     await this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  public async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'nice cock';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
   }
 }
