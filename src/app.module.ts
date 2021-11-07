ea75959528acb5b3fbfed86a88b264d1a8087658
ihorpidhornyi <ihorp@rabota.ua>
2021-11-07 11:03:43 +0200

    feat(app): changes for heroku deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,18 +15,26 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isProd = configService.get('STAGE') === 'prod';
+        return {
+          ssl: isProd,
+          extra: {
+            ssl: isProd ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+        };
+      },
     }),
     AuthModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+}
